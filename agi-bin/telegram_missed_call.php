5a4f7632278e1c8085bb36da056a7fa0521ecf76
markeloviv <markelov_i@mail.ru>
2021-03-08 03:20:52 +0300

    Add files via upload

--- agi-bin/telegram_missed_call.php
+++ agi-bin/telegram_missed_call.php
@@ -3,59 +3,111 @@
     require('phpagi.php');
     $agi = new AGI();
 
+    $cid = $agi->request['agi_callerid'];
+    $exttolocal = $agi->request['agi_arg_6'];
+    $dialstatus = $agi->request['agi_arg_4'];
+    $vmstatus = $agi->request['agi_arg_5'];
+
     $telegramstatus = $agi->request['agi_arg_3'];
 
-    if ($telegramstatus == "enabled") {
-        $cid = $agi->request['agi_callerid'];
+    $datetime = date("d.m.Y H:i:s");
+
+    sleep(3);
+
+    if ($vmstatus == "SUCCESS" && $telegramstatus == "enabled") {
         $token = $agi->request['agi_arg_1'];
         $token = str_replace(" ", ":", $token);
         $chatid = $agi->request['agi_arg_2'];
-        $dialstatus = $agi->request['agi_arg_4'];
-        $vmstatus = $agi->request['agi_arg_5'];
-        $exttolocal = $agi->request['agi_arg_6'];
-        
-        $datetime = date("d.m.Y H:i:s");
+        $voicemail = $agi->request['agi_arg_13'];
+            
+        $mess = "Missed call + voice message\n".$datetime."\n".$cid." -> ".$exttolocal;
+        //$path = '/var/spool/asterisk/voicemail/default/'.$exttolocal.'/INBOX/';
+        $path = '/var/spool/asterisk/voicemail/default/'.$voicemail.'/INBOX/';
+        $fname = find_last_file($path, '.wav');
+        if ($fname != '') {
+            sendDocumentTel($mess, $path.$fname);
+        }
+    } elseif ($dialstatus != "ANSWER" && $telegramstatus == "enabled") {
+        $token = $agi->request['agi_arg_1'];
+        $token = str_replace(" ", ":", $token);
+        $chatid = $agi->request['agi_arg_2'];
 
-        $mess = "Missed call\n".$datetime."\n".$cid;
+        $mess = "Missed call\n".$datetime."\n".$cid." -> ".$exttolocal;
+        sendMessageTel($mess);
+    } elseif ($dialstatus == "ANSWER") {
+        $path = '/var/spool/asterisk/monitor/'.date("Y").'/'.date("m").'/'.date("d").'/';
+        $fname = find_last_file($path, $agi->request['agi_uniqueid'].'.wav');
 
-        if ($vmstatus == "SUCCESS") {
-            $mess .= "\nvoice message";
-            $fname = "/var/spool/asterisk/voicemail/default/".$exttolocal."/INBOX/".find_latest($exttolocal);
+        $callstatus = $agi->request['agi_arg_7'];
+        if ($callstatus == "enabled") {
+            $calltoken = $agi->request['agi_arg_8'];
+            $token = str_replace(" ", ":", $calltoken);
+            $chatid = $agi->request['agi_arg_9'];
+
+            $mess = "Call\n".$datetime."\n".$cid." -> ".$exttolocal;
+            sendMessageTel($mess);
+        }
+
+        $rcstatus = $agi->request['agi_arg_10'];
+        if ($fname != '' && $rcstatus == "enabled") {
+            $rctoken = $agi->request['agi_arg_11'];
+            $token = str_replace(" ", ":", $rctoken);
+            $chatid = $agi->request['agi_arg_12'];
+
+            $mess = "Record call\n".$datetime."\n".$cid." -> ".$exttolocal;
+            sendDocumentTel($mess, $path.$fname);
+        }
+    }
+
+    function sendDocumentTel($mess, $fname) {
+        global $token, $chatid;
+        $schatid = explode("-", $chatid);
+        foreach( $schatid as $c ) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendDocument?chat_id=".$chatid."&caption=".urlencode($mess));
+            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendDocument?chat_id=".$c."&caption=".urlencode($mess));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_POST, 1);
             $finfo = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $fname);
             $cFile = new CURLFile($fname, $finfo);
             curl_setopt($ch, CURLOPT_POSTFIELDS, [
-                    "document" => $cFile
+                "document" => $cFile
             ]);
             $result = curl_exec($ch);
             var_dump($result);
             curl_close($ch);
-        } elseif ($dialstatus != "ANSWER") {
+            sleep(3);
+        }
+    }
+
+    function sendMessageTel($mess) {
+        global $token, $chatid;
+        $schatid = explode("-", $chatid);
+        foreach( $schatid as $c ) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chatid."&text=".urlencode($mess));
+            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$c."&text=".urlencode($mess));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_POST, 1);
             $result = curl_exec($ch);
             var_dump($result);
             curl_close($ch);
+            sleep(3);
         }
     }
 
-    function find_latest($ext) {
+    function find_last_file($path, $s) {
         $l = 0;
         $r = '';
-        foreach( new DirectoryIterator('/var/spool/asterisk/voicemail/default/'.$ext.'/INBOX/') as $file ) {
+        foreach( new DirectoryIterator($path) as $file ) {
             $ctime = $file->getCTime();
             $fname = $file->getFileName();
             if( $ctime > $l ) {
                 $r = $fname;
-                $pos = strpos($r, '.wav');
+                $pos = strpos($r, $s);
                 
                 if ($pos !== false) {
                     $l = $ctime;
+                } else {
+                    $r = '';
                 }
             }
         }
--- functions.inc.php
+++ functions.inc.php
@@ -68,11 +68,27 @@ function telegrammissedcall_get_config($engine) {
   $token = $token1.' '.$token2;
   $chatid = '${DB(AMPUSER/${EXTTOCALL}/telegrammissedcall/telegram)}';
   $telegramstatus = '${DB(AMPUSER/${EXTTOCALL}/telegrammissedcall/status)}';
+  $telegramsvoicemail = '${DB(AMPUSER/${EXTTOCALL}/telegrammissedcall/voicemail)}';
+
   $dialstatus = '${DIALSTATUS}';
   $vmstatus = '${VMSTATUS}';
   $exttolocal = '${EXTTOCALL}';
 
-  $ext->add($mcontext,$exten,'', new ext_AGI('telegram_missed_call.php,'.$token.','.$chatid.','.$telegramstatus.','.$dialstatus.','.$vmstatus.','.$exttolocal.','.$test.''));
+  $callstatus = '${DB(AMPUSER/${EXTTOCALL}/telegramcall/status_call)}';
+  $calltoken1 = '${DB(AMPUSER/${EXTTOCALL}/telegramcall/bot1_call)}';
+  $calltoken2 = '${DB(AMPUSER/${EXTTOCALL}/telegramcall/bot2_call)}';
+  $calltoken = $calltoken1.' '.$calltoken2;
+  $callchatid = '${DB(AMPUSER/${EXTTOCALL}/telegramcall/telegram_call)}';
+
+  $rcstatus = '${DB(AMPUSER/${EXTTOCALL}/telegramrecordcall/status_rc)}';
+  $rctoken1 = '${DB(AMPUSER/${EXTTOCALL}/telegramrecordcall/bot1_rc)}';
+  $rctoken2 = '${DB(AMPUSER/${EXTTOCALL}/telegramrecordcall/bot2_rc)}';
+  $rctoken = $rctoken1.' '.$rctoken2;
+  $rchatid = '${DB(AMPUSER/${EXTTOCALL}/telegramrecordcall/telegram_rc)}';
+
+
+
+  $ext->add($mcontext,$exten,'', new ext_AGI('telegram_missed_call.php,'.$token.','.$chatid.','.$telegramstatus.','.$dialstatus.','.$vmstatus.','.$exttolocal.','.$callstatus.','.$calltoken.','.$callchatid.','.$rcstatus.','.$rctoken.','.$rchatid.','.$telegramsvoicemail));
 }
 
 
@@ -121,17 +137,44 @@ function telegrammissedcall_configpageload() {
   $action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
   $extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
 
+  $mcn0 = telegramcall_getall($extdisplay);
+  $section0 = _('Call Notifications');
+  $telegramcall_label =      _("Notifications");
+  $telegramcall_telegram_label =    _("Telergram ID");
+  $telegramcall_bot_label =    _("Telegram Bot token");
+  $telegramcall_tt = _("Enable notification of calls");
+  $telegramcall_pt = _("Here you can specify the telegram user ID (personal) or the general chat ID. You can specify multiple separated dashes (111-222-333).");
+
+  $currentcomponent->addguielem($section0, new gui_selectbox('telegramcall_status', $currentcomponent->getoptlist('telegrammissedcall_status'), $mcn0['telegramcall_status'], $telegramcall_label, $telegramcall_tt, '', false));
+  $currentcomponent->addguielem($section0, new gui_textbox('telegramcall_bot', $mcn0['telegramcall_bot'],$telegramcall_bot_label, '', '' , false));
+  $currentcomponent->addguielem($section0, new gui_textbox('telegramcall_telegram', $mcn0['telegramcall_telegram'],$telegramcall_telegram_label, $telegramcall_pt, '' , false));
+
+
   $mcn = telegrammissedcall_getall($extdisplay);
   $section = _('Missed Call Notifications');
   $telegrammissedcall_label =      _("Notifications");
   $telegrammissedcall_telegram_label =    _("Telergram ID");
   $telegrammissedcall_bot_label =    _("Telegram Bot token");
+  $telegrammissedcall_voicemail_label =    _("VoiceMail Number Extension");
   $telegrammissedcall_tt = _("Enable notification of missed calls");
-  $telegrammissedcall_pt = _("Here you can specify the telegram user id(personal) or the general chat id.");
+  $telegrammissedcall_pt = _("Here you can specify the telegram user ID (personal) or the general chat ID. You can specify multiple separated dashes (111-222-333).");
   
   $currentcomponent->addguielem($section, new gui_selectbox('telegrammissedcall_status', $currentcomponent->getoptlist('telegrammissedcall_status'), $mcn['telegrammissedcall_status'], $telegrammissedcall_label, $telegrammissedcall_tt, '', false));
   $currentcomponent->addguielem($section, new gui_textbox('telegrammissedcall_bot', $mcn['telegrammissedcall_bot'],$telegrammissedcall_bot_label, '', '' , false));
   $currentcomponent->addguielem($section, new gui_textbox('telegrammissedcall_telegram', $mcn['telegrammissedcall_telegram'],$telegrammissedcall_telegram_label, $telegrammissedcall_pt, '' , false));
+  $currentcomponent->addguielem($section, new gui_textbox('telegrammissedcall_voicemail', $mcn['telegrammissedcall_voicemail'],$telegrammissedcall_voicemail_label, '', '' , false));
+
+  $mcn2 = telegramrecordcall_getall($extdisplay);
+  $section2 = _('Call recording notifications');
+  $telegramrecordcall_label =      _("Notifications");
+  $telegramrecordcall_telegram_label =    _("Telergram ID");
+  $telegramrecordcall_bot_label =    _("Telegram Bot token");
+  $telegramrecordcall_tt = _("Enable notification of record calls");
+  $telegramrecordcall_pt = _("Here you can specify the telegram user ID (personal) or the general chat ID. You can specify multiple separated dashes (111-222-333).");
+
+  $currentcomponent->addguielem($section2, new gui_selectbox('telegramrecordcall_status', $currentcomponent->getoptlist('telegrammissedcall_status'), $mcn2['telegramrecordcall_status'], $telegramrecordcall_label, $telegramrecordcall_tt, '', false));
+  $currentcomponent->addguielem($section2, new gui_textbox('telegramrecordcall_bot', $mcn2['telegramrecordcall_bot'],$telegramrecordcall_bot_label, '', '' , false));
+  $currentcomponent->addguielem($section2, new gui_textbox('telegramrecordcall_telegram', $mcn2['telegramrecordcall_telegram'],$telegramrecordcall_telegram_label, $telegramrecordcall_pt, '' , false));
 }
 
 function telegrammissedcall_configprocess() {
@@ -141,17 +184,40 @@ function telegrammissedcall_configprocess() {
   $ext = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
   $extn = isset($_REQUEST['extension'])?$_REQUEST['extension']:null;
 
+  $mcn0=array();
+  $mcn0['status_call'] =      isset($_REQUEST['telegramcall_status']) ? $_REQUEST['telegramcall_status'] : 'disabled';
+  $mcn0['telegram_call'] =    isset($_REQUEST['telegramcall_telegram']) ? $_REQUEST['telegramcall_telegram'] : 'enabled';
+  $mcn0['bot_call'] =    isset($_REQUEST['telegramcall_bot']) ? $_REQUEST['telegramcall_bot'] : 'enabled';
+  $mcn0['bot1_call'] =    isset($_REQUEST['telegramcall_bot']) ? explode(":", $_REQUEST['telegramcall_bot'])[0] : 'enabled';
+  $mcn0['bot2_call'] =    isset($_REQUEST['telegramcall_bot']) ? explode(":", $_REQUEST['telegramcall_bot'])[1] : 'enabled';
+
   $mcn=array();
   $mcn['status'] =      isset($_REQUEST['telegrammissedcall_status']) ? $_REQUEST['telegrammissedcall_status'] : 'disabled';
   $mcn['telegram'] =    isset($_REQUEST['telegrammissedcall_telegram']) ? $_REQUEST['telegrammissedcall_telegram'] : 'enabled';
   $mcn['bot'] =    isset($_REQUEST['telegrammissedcall_bot']) ? $_REQUEST['telegrammissedcall_bot'] : 'enabled';
   $mcn['bot1'] =    isset($_REQUEST['telegrammissedcall_bot']) ? explode(":", $_REQUEST['telegrammissedcall_bot'])[0] : 'enabled';
   $mcn['bot2'] =    isset($_REQUEST['telegrammissedcall_bot']) ? explode(":", $_REQUEST['telegrammissedcall_bot'])[1] : 'enabled';
+  $mcn['voicemail'] =    isset($_REQUEST['telegrammissedcall_voicemail']) ? $_REQUEST['telegrammissedcall_voicemail'] : 'enabled';
+
+  $mcn2=array();
+  $mcn2['status_rc'] =      isset($_REQUEST['telegramrecordcall_status']) ? $_REQUEST['telegramrecordcall_status'] : 'disabled';
+  $mcn2['telegram_rc'] =    isset($_REQUEST['telegramrecordcall_telegram']) ? $_REQUEST['telegramrecordcall_telegram'] : 'enabled';
+  $mcn2['bot_rc'] =    isset($_REQUEST['telegramrecordcall_bot']) ? $_REQUEST['telegramrecordcall_bot'] : 'enabled';
+  $mcn2['bot1_rc'] =    isset($_REQUEST['telegramrecordcall_bot']) ? explode(":", $_REQUEST['telegramrecordcall_bot'])[0] : 'enabled';
+  $mcn2['bot2_rc'] =    isset($_REQUEST['telegramrecordcall_bot']) ? explode(":", $_REQUEST['telegramrecordcall_bot'])[1] : 'enabled';
 
   if ($ext==='') {
     $extdisplay = $extn;
   } else {
     $extdisplay = $ext;
+  }
+
+  if ($action == "add" || $action == "edit" || (isset($mcn0['callnotify']) && $mcn0['callnotify']=="false")) {
+    if (!isset($GLOBALS['abort']) || $GLOBALS['abort'] !== true) {
+      telegramcall_update($extdisplay, $mcn0);
+    }
+  } elseif ($action == "del") {
+    telegramcall_del($extdisplay);
   }
 
   if ($action == "add" || $action == "edit" || (isset($mcn['misedcallnotify']) && $mcn['misedcallnotify']=="false")) {
@@ -161,6 +227,35 @@ function telegrammissedcall_configprocess() {
   } elseif ($action == "del") {
     telegrammissedcall_del($extdisplay);
   }
+
+  if ($action == "add" || $action == "edit" || (isset($mcn2['recordcallnotify']) && $mcn2['recordcallnotify']=="false")) {
+    if (!isset($GLOBALS['abort']) || $GLOBALS['abort'] !== true) {
+      telegramrecordcall_update($extdisplay, $mcn2);
+    }
+  } elseif ($action == "del") {
+    telegramrecordcall_del($extdisplay);
+  }
+}
+
+
+function telegramcall_getall($ext, $base='AMPUSER') {
+  global $amp_conf;
+  global $astman;
+  $mcn=array();
+
+  if ($astman) {
+    $telegramcall_status = telegramcall_get($ext,"status_call", $base);
+    $mcn['telegramcall_status'] = $telegramcall_status ? $telegramcall_status : 'disabled';
+    $telegramcall_telegram = telegramcall_get($ext,"telegram_call", $base);
+    $mcn['telegramcall_telegram'] = $telegramcall_telegram ?  $telegramcall_telegram : '';
+    $telegramcall_bot = telegramcall_get($ext,"bot_call", $base);
+    $mcn['telegramcall_bot'] = $telegramcall_bot ?  $telegramcall_bot : '';
+
+
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+  return $mcn;
 }
 
 function telegrammissedcall_getall($ext, $base='AMPUSER') {
@@ -175,12 +270,47 @@ function telegrammissedcall_getall($ext, $base='AMPUSER') {
     $mcn['telegrammissedcall_telegram'] = $telegrammissedcall_telegram ?  $telegrammissedcall_telegram : '';
     $telegrammissedcall_bot = telegrammissedcall_get($ext,"bot", $base);
     $mcn['telegrammissedcall_bot'] = $telegrammissedcall_bot ?  $telegrammissedcall_bot : '';
+    $telegrammissedcall_voicemail = telegrammissedcall_get($ext,"voicemail", $base);
+    $mcn['telegrammissedcall_voicemail'] = $telegrammissedcall_voicemail ?  $telegrammissedcall_voicemail : '';
 
 
   } else {
     fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
   }
   return $mcn;
+}
+
+function telegramrecordcall_getall($ext, $base='AMPUSER') {
+  global $amp_conf;
+  global $astman;
+  $mcn=array();
+
+  if ($astman) {
+    $telegramrecordcall_status = telegramrecordcall_get($ext,"status_rc", $base);
+    $mcn['telegramrecordcall_status'] = $telegramrecordcall_status ? $telegramrecordcall_status : 'disabled';
+    $telegramrecordcall_telegram = telegramrecordcall_get($ext,"telegram_rc", $base);
+    $mcn['telegramrecordcall_telegram'] = $telegramrecordcall_telegram ?  $telegramrecordcall_telegram : '';
+    $telegramrecordcall_bot = telegramrecordcall_get($ext,"bot_rc", $base);
+    $mcn['telegramrecordcall_bot'] = $telegramrecordcall_bot ?  $telegramrecordcall_bot : '';
+
+
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+  return $mcn;
+}
+
+
+function telegramcall_get($ext, $key, $base='AMPUSER', $sub='telegramcall') {
+  global $astman;
+  global $amp_conf;
+
+  if ($astman) {
+    if(!empty($sub) && $sub!=false)$key=$sub.'/'.$key;
+    return $astman->database_get($base,$ext.'/'.$key);
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
 }
 
 function telegrammissedcall_get($ext, $key, $base='AMPUSER', $sub='telegrammissedcall') {
@@ -195,6 +325,37 @@ function telegrammissedcall_get($ext, $key, $base='AMPUSER', $sub='telegrammisse
   }
 }
 
+function telegramrecordcall_get($ext, $key, $base='AMPUSER', $sub='telegramrecordcall') {
+  global $astman;
+  global $amp_conf;
+
+  if ($astman) {
+    if(!empty($sub) && $sub!=false)$key=$sub.'/'.$key;
+    return $astman->database_get($base,$ext.'/'.$key);
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+}
+
+
+
+
+
+
+
+function telegramcall_update($ext, $options, $base='AMPUSER', $sub='telegramcall') {
+  global $astman;
+  global $amp_conf;
+
+  if ($astman) {
+    foreach ($options as $key => $value) {
+      if(!empty($sub) && $sub!=false)$key=$sub.'/'.$key;
+      $astman->database_put($base,$ext."/$key",$value);
+    }
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+}
 
 function telegrammissedcall_update($ext, $options, $base='AMPUSER', $sub='telegrammissedcall') {
   global $astman;
@@ -210,7 +371,46 @@ function telegrammissedcall_update($ext, $options, $base='AMPUSER', $sub='telegr
   }
 }
 
+function telegramrecordcall_update($ext, $options, $base='AMPUSER', $sub='telegramrecordcall') {
+  global $astman;
+  global $amp_conf;
+
+  if ($astman) {
+    foreach ($options as $key => $value) {
+      if(!empty($sub) && $sub!=false)$key=$sub.'/'.$key;
+      $astman->database_put($base,$ext."/$key",$value);
+    }
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+}
+
+
+function telegramcall_del($ext, $base='AMPUSER', $sub='telegramcall') {
+  global $astman;
+  global $amp_conf;
+
+  // Clean up the tree when the user is deleted
+  if ($astman) {
+    $astman->database_deltree("$base/$ext/$sub");
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+}
+
 function telegrammissedcall_del($ext, $base='AMPUSER', $sub='telegrammissedcall') {
+  global $astman;
+  global $amp_conf;
+
+  // Clean up the tree when the user is deleted
+  if ($astman) {
+    $astman->database_deltree("$base/$ext/$sub");
+  } else {
+    fatal("Cannot connect to Asterisk Manager with ".$amp_conf["AMPMGRUSER"]."/".$amp_conf["AMPMGRPASS"]);
+  }
+}
+
+function telegramrecordcall_del($ext, $base='AMPUSER', $sub='telegramrecordcall') {
   global $astman;
   global $amp_conf;
 
